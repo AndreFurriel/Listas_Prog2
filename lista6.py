8a515d5ea9e00a1679f37f7a6b6adff00bf288e9
AndreFurriel <al.andre.oliveira@impatech.edu.br>
2025-10-30 22:18:52 -0300

    Update lista6.py

--- lista6.py
+++ lista6.py
@@ -64,7 +64,34 @@ sempre dobra o comprimento do array, enquanto no python esse valor varia,
 e retorna um novo array, enquanto o python altera o antigo.
 """
 #Questão2
-
+"""Explique o que a função find_nb faz, e analise qual a sua complexidade."""
+"""
+Encontra o ponto "idt" de um array "data", mais próximo do ponto "point"
+    Calcula a distancia entre os pontos do array "data" e do ponto "point"
+        Com subtração de arrays: O(n)
+    Cria um array "d" com as normas das diferenças entre os pontos do array "data" e o ponto "point"
+        Com linalg.norm: O(n)
+    Encontra o indice "idt" do ponto com diferença minima do array "d"
+        Com argmin: O(n)
+    Retorna a distância minima "d[idt]" e o indice "idt": O(1)
+    ComplexidadeTotal = O(n) + O(n) + O(n) + O(1) = O(3n + 1) = O(n)
+"""
+from matplotlib import pyplot as plt
+import scipy as sp
+numpy.random.seed(1001)
+normaldist1 = sp.stats.gamma(2, 4)
+normaldist2 = sp.stats.gamma(4, 1)
+X = normaldist1.rvs(200)
+Y = normaldist2.rvs(200)
+data  = numpy.array([X, Y]).T
+def find_nb(data, point):
+    Dt = data - point
+    d = numpy.linalg.norm(Dt, axis=1)
+    idt = numpy.argmin(d)
+    return d[idt], idt
+point = numpy.array([8.5, 4.5])
+dd, idx = find_nb(data, point)
+print(f"O ponto mais proximo está a {dd:.2f} de distância, é o ponto ({data[idx,0]:.2f},{data[idx,1]:.2f})  no índice {idx}")
 #Questão3
 
 #Questão4
