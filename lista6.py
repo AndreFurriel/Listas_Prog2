6e0e151a4a9a6f105180bde58783f615f6196e3a
AndreFurriel <al.andre.oliveira@impatech.edu.br>
2025-10-30 21:31:14 -0300

    Update lista6.py

---
+++ lista6.py
@@ -0,0 +1,72 @@
+#Questão1
+import numpy
+class My_Array(numpy.ndarray):
+    """
+    Uma subclasse customizada de numpy.array que adiciona o método append
+    
+    Métodos:
+        append (float): Adiciona um item ao final do array
+    """
+    def __new__(cls, input_array):
+        """
+        Cria um novo My_Array
+        
+        Argumentos:
+            input_array (array-like): Array a ser convertido.
+        
+        Retorna:
+            My_Array: My_Array criado
+        """
+        obj = numpy.asarray(input_array).view(cls)
+        obj.sig = obj.size
+        return obj
+    def append(self, item):
+        """Adiciona um item ao final do array
+
+        Args:
+            item (float): Adiciona um item ao final do array
+
+        Returns:
+            My_Array: Novo array com o item no final
+        """
+        if self.sig == 0:
+            aux = My_Array([float(item)])
+        elif self.sig == self.size:
+            p = 1
+            while p <= self.sig:
+                p = 2*p
+            l = [0.0]*(p)
+            aux = My_Array(l)
+            for i in range(self.sig):
+                aux[i] = self[i]
+            aux[self.sig] = item
+            aux.sig = self.sig + 1
+        elif self.sig < self.size:
+            self[self.sig] = item
+            self.sig = self.sig + 1
+            aux = self
+        return aux
+    def __str__(self):
+        return str(self[:self.sig].tolist())
+a = My_Array([])
+a = a.append(1)
+print(a.sig)
+print(a)
+a = a.append(2.1)
+print(a.sig)
+print(a)
+a = a.append(3.21)
+print(a.sig)
+print(a)
+"""
+Minha implementação do append trabalha com tipo fixo (float),
+sempre dobra o comprimento do array, enquanto no python esse valor varia,
+e retorna um novo array, enquanto o python altera o antigo.
+"""
+#Questão2
+
+#Questão3
+
+#Questão4
+
+#Questão5
