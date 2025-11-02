3af3e74f6fac413e89ce869ab0eca1effb3bdab5
AndreFurriel <al.andre.oliveira@impatech.edu.br>
2025-11-02 19:55:33 -0300

    Update lista7.py

--- lista7.py
+++ lista7.py
@@ -1,4 +1,18 @@
 #Questão 1
+"""
+Considere o seguinte cenário: você está encarregado de criar um programa em Python para lidar com medidas
+que são expressas em centenas de quilômetros, mas exigem uma precisão de milímetros.
+Por exemplo, isso poderia ser aplicado para representar as coordenadas exatas de componentes em uma planta industrial,
+como a posição de um parafuso. Discuta como você abordaria esse problema.
+Dica: você pode criar a sua própria classe numérica.
+"""
+class posicao():
+    def __init__(self, km:int, m:int, mm:int):
+        self.km = km
+        self.m = m
+        self.mm = mm
+        
+parafuso = posicao(123,456,789)
 
 #Questão 2
 
