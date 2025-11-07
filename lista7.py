952aeabc3eb82957f7e63b17c3298f8fed3805d7
AndreFurriel <al.andre.oliveira@impatech.edu.br>
2025-11-07 08:58:25 -0300

    Update lista7.py

--- lista7.py
+++ lista7.py
@@ -41,7 +41,6 @@ class Domain:
     def copy(self):
         raise NotImplementedError 
 
-
 class Interval(Domain):
     def __init__(self, p1, p2):
         self.inff, self.supp = min(p1, p2), max(p1, p2)
@@ -74,7 +73,6 @@ class Interval(Domain):
     def copy(self):
         return Interval(self.inff, self.supp)
 
-
 class RealFunction:
     f = None
     prime = None
@@ -101,7 +99,6 @@ class RealFunction:
         Y = self(X)
         ax.plot(X,Y)
         return fig, ax
-
 
 def bissect(f: RealFunction, 
             search_space: Interval, 
@@ -131,7 +128,6 @@ def bissect(f: RealFunction,
             ss = Interval(m, b)
     return ss
 
-
 def grid_search(f: RealFunction, domain: Interval = None, grid_freq = 8) -> Interval:
     if domain is not None:
         D = domain.copy()
@@ -145,7 +141,6 @@ def grid_search(f: RealFunction, domain: Interval = None, grid_freq = 8) -> Inte
         return None
     idx = np.argmax(VI)
     return Interval(L1[idx], L1[idx+1])
-
 
 def newton_root(f: RealFunction, 
             search_space: Interval, 
@@ -172,7 +167,6 @@ def newton_root(f: RealFunction,
             return m
     return m
 
-
 if __name__ == '__main__':
     import sympy as sp
 
@@ -196,7 +190,87 @@ if __name__ == '__main__':
     print()
     print(newton_root(ft, search_space=ND))
 #Questão 3
+"""
+Estenda o código CodigosExcerciciosProvas/poly_interpolant.py com uma classe de interpolante que construa o polinômio de Lagrange
+usando o Scipy.
+Compare a classe VandermondeMatrix com essa nova implementação fazendo um código que compare a velocidade da construção dos Polinômios.
+"""
+import numpy as np
+import sympy
 
+class interpolater:
+
+    def evaluate(self, X):
+        raise NotImplementedError
+
+    def __call__(self,  X):
+        return self.evaluate(X)
+
+class VandermondeMatrix(interpolater):
+    def __init__(self, x, y):
+        if len(x) != len(y):
+            raise RuntimeError(f"Dimensions must be equal len(x) = {len(x)} != len(y) = {len(y)}")
+        self.data = [x, y]
+        self._degree = len(x) -1
+        self._buildMatrix()
+        self._poly = np.linalg.solve(self.matrix, self.data[1])
+
+    def _buildMatrix(self):
+        self.matrix = np.ones([self._degree+1, self._degree+1])
+        for i, x in enumerate(self.data[0]):
+            self.matrix[i, 1:] = np.multiply.accumulate(np.repeat(x, self._degree))
+    
+    def evaluate(self, X):
+        r = 0.0
+        for c in self._poly[::-1]:
+            r = c+r*X
+        return r
+    
+class LagrangePolinomial(interpolater):
+    def __init__(self, x, y):
+        if len(x) != len(y):
+            raise RuntimeError(f"Dimensions must be equal len(x) = {len(x)} != len(y) = {len(y)}")
+        self.data = [x, y]
+        self._degree = len(x) -1
+        self.x = scipy.
+        self._poly = 
+
+    def form(self):
+        s = 0
+        for j in range(self._degree):
+            m = 1
+            for i in range(self._degree):
+                x = sympy.symbols("x")
+                (x - self.data[0][i]) / (self.data[0][j] - self.data[0][i])
+                m = m *
+            s = s + self.data[1][j] *
+
+def random_sample(intv, N):
+    r = np.random.uniform(intv[0], intv[1], N-2)
+    r.sort()
+    return np.array([intv[0]] + list(r) + [intv[1]])
+
+def error_pol(f, P, intv, n = 1000):
+    x = random_sample(intv, n)
+    vectError = np.abs(f(x)-P(x))
+    return np.sum(vectError)/n, np.max(vectError)
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    DataX = [10.7       , 11.075     , 11.45      , 11.825     , 12.2       , 12.5]
+    DataY = [-0.25991903,  0.04625002,  0.16592075,  0.13048074,  0.13902777, 0.2]
+
+    Pvm = VandermondeMatrix(DataX, DataY)
+
+    X = np.linspace(min(DataX)-0.2, max(DataX)+0.2, 100)
+    Y = Pvm(X)
+
+    _, ax = plt.subplots(1)
+    ax.plot(X,Y)
+    ax.axis('equal')
+    ax.plot(DataX, DataY, 'o')
+    plt.show()
 #Questão 4
 
 #Questão 5
